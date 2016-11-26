3e81ba9e1ed5df96fae8fe9a9f17080e99c1566f
王喆 <nasa127@gmail.com>
2016-11-27 01:43:56 +0800

    (っ˘▽˘)っ nasa ⊂(◕。◕⊂) 小心 window.requestAnimationFrame

--- src/modules/collect/views/home.jsx
+++ src/modules/collect/views/home.jsx
@@ -17,18 +17,37 @@ const Home = React.createClass({
     getInitialState: function() {
         return {showInfo: false};
     },
+    componentDidMount() {
+        let w = 572
+        let h = 445
+
+        var sprite = new window.TiledImage(require('../../../img/kv_anim.png'), 3, 1);
+        sprite.changeRow(0);
+        sprite.changeColumnInterval(0, 2);
+        var ctx = document.getElementById("canvas").getContext("2d");
+        tick();
+        function tick() {
+            ctx.clearRect(0, 0, w, h);
+            sprite.tick(ctx, 572/2, 445/2);
+            window.requestAnimationFrame(tick);
+        }
+    },
     render() {
         let minHeight = window.innerHeight
+        let w = 572
+        let h = 445
         return (
             <div className='homeView' style={{
                 minHeight: `${minHeight}px`
             }}>
                 <HeadLogo/>
                 <div className='container'>
-                    <img className='kv' src={require('../../../img/main_kv_1.png')}/>
+
+                    <canvas className='kv' ref='kv' width={w} height={h} id="canvas"></canvas>
+
                     <div className='btns'>
                         <img className='btnJoin' src={require('../../../img/btn_join.png')}/>
-                        <img onClick={ this.doShowInfo } className='btnInfo' src={require('../../../img/btn_info.png')}/>
+                        <img onClick={this.doShowInfo} className='btnInfo' src={require('../../../img/btn_info.png')}/>
                     </div>
                     <Popup show={this.state.showInfo} closePopup={this.doCloseInfo}>
                         <div style={{
@@ -36,7 +55,7 @@ const Home = React.createClass({
                         }}>
                             <div className='infoContainer'>
                                 <div className='activityInfoBg'>
-                                    <img onClick={ this.doCloseInfo } className='btnBack' src={require('../../../img/btn_back.png')}/>
+                                    <img onClick={this.doCloseInfo} className='btnBack' src={require('../../../img/btn_back.png')}/>
                                 </div>
                             </div>
                         </div>
