c07ebccc92c08f26e27c43eaba88a4ca95acd340
王喆 <nasa127@gmail.com>
2016-11-27 20:06:33 +0800

    dfc 首页完成

--- src/modules/dfc/views/home.jsx
+++ src/modules/dfc/views/home.jsx
@@ -3,12 +3,21 @@
  */
 
 import React from 'react'
+import ReactDOM from 'react-dom'
+import HeadLogo from '../../common/head-logo.jsx'
 
 const Home = React.createClass({
     render() {
+        let minHeight = window.innerHeight
         return (
-            <div>
-                DFC home view
+            <div className='DFChomeView' style={{
+                minHeight: `${minHeight}px`
+            }}>
+                <HeadLogo/>
+                <div className='container'>
+                    <img className='join' src={require('../../../img/dfc_join.png')} />
+                    <img className='auth' src={require('../../../img/dfc_auth.png')} />
+                </div>
             </div>
         )
     }
